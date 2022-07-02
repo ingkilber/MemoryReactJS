e7993ce3aa3c7d721596486203dabaaea6c4f7bf
Kilber Marcano <94709159+ingkilber@users.noreply.github.com>
2022-07-02 00:07:06 -0300

    duplicado de array

--- src/App.js
+++ src/App.js
@@ -4,80 +4,66 @@ import "./App.css";
 import Card from "./components/card";
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+
+  // limitar o solo indicar cuantos obj o items que deseo traer de la api ?limit=2 => se le llama parametros get
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=12";
 
   const [cards, setCards] = useState([]);
+  
+
+  // Desordenar imagenes
+  // const shuffleArray = (array) => {
+  //   for (let i = array.length - 1; i > 0; i--) {
+  //     let j = Math.floor(Math.random() * (i + 1));
+  //     let temp = array[i];
+  //     array[i] = array[j];
+  //     array[j] = temp;
+  //   }
+  // }
+
 
   const fetchApi = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    setCards(data.results);
+
+    // multiplicar dos array para obtener un resultado duplicado
+
+let multiplicar = data.results;
+let unir = multiplicar.concat(multiplicar);
+
+    setCards(unir);
 
     // console.log(data.results);
 
-
-
-    data.results.forEach((element) => {
-      // Aqui realizo el frects, la consulta para obtener datos
-      fetch("https://pokeapi.co/api/v2/pokemon/"+element.name)
-      //obtiene la respuestas
-        .then((resp) => resp.json())
-        //funcion anonima
-        .then(function (data) {
-          // console.log(data.sprites.other.home.front_default)
-          // let authors = data.results;
-        })
-        .catch(function (error) {
-          // console.log(error);
-        });
-
-      // console.log(element.name);
-    });
-
-    // extraer imagenes
-
-    // const responseimg = await fetch ("https://pokeapi.co/api/v2/pokemon/")
-    // const dataimg = await responseimg.json ();
-
-    // console.log(dataimg.sprites.other)
-
-    // fin de extraer imagenes
+    // data.results.forEach((element) => {
+    //   // Aqui realizo el frects, la consulta para obtener datos
+    //   fetch("https://pokeapi.co/api/v2/pokemon/"+element.name)
+    //   //obtiene la respuestas
+    //     .then((resp) => resp.json())
+    //     //funcion anonima
+    //     .then(function (data) {
+    //       // console.log(data.sprites.other.home.front_default)
+    //       // let authors = data.results;
+    //     })
+    //     .catch(function (error) {
+    //       // console.log(error);
+    //     });
+    //   // console.log(element.name);
+    // });
   };
 
   useState(() => {
     fetchApi();
   }, []);
-
-  // useEffect (() => {
-  //   fetchApi ()
-  // }, [])
-
-  //api funcionando
-  // fetch("mock.codes/500")
-  // .then(() => console.log("Todo bien"))
-  // .catch(() => console.log("Algo falló"))
-  // const [cards, setCards] = useState();
+  
 
   const listItems = cards.map((todo) => (
     <li className="cards" key={todo.name}>
       <div>
         <Card kilber={todo.name} />
-        {/* frontFace={todo.sprites.other.home.front_default} */}
       </div>
     </li>
   ));
-
-
-  // const listItems2 = cards.forEach(element => {
-  //   <li className="cartas" key={element.name}>
-  //     <div>
-  //       hola
-  //     </div>
-      
-  //     {element.name}
-  //   </li>
-    
-  // });
 
   return (
     <div>
