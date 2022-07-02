371a851f790e093d278c1667ab03cb6a7b91c959
Kilber Marcano <94709159+ingkilber@users.noreply.github.com>
2022-07-02 20:56:43 -0300

    Error por Solucionar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
   const url = "https://pokeapi.co/api/v2/pokemon/?limit=10";
 
   const [cards, setCards] = useState([]);
+  const [firstCard, setFirstCard] = useState ({});
+  const [secondCard, setSecondCard] = useState ({});
 
+  const [unflippedCards, setUnflippedCards] = useState([]);
+  const [disabledCards, setDisabledCards] = useState([]);
 
   const fetchApi = async () => {
     const response = await fetch(url);
@@ -55,12 +59,58 @@ setCards(unir);
     fetchApi();
   }, []);
 
-  
+  //nos dice que va a funcionar cada ves que tenga una segunda carta
+  useEffect(() => {
+    checkForMatch();
+  }, [secondCard]);
+
+  //metodo para voltear las cartas
+  const flipCard = (name, number) => {
+    if (firstCard.name === name && firstCard.number === number) {
+      return 0;
+    }
+    if (!firstCard.name) {
+      setFirstCard({ name, number });
+    }
+    else if (!secondCard.name) {
+      setSecondCard({ name, number });
+    }
+    return 1;
+  }
+
+// Saber si una carta es igual a otra
+  const checkForMatch = () => {
+    if (firstCard.name && secondCard.name) {
+      const match = firstCard.name === secondCard.name;
+      match ? disableCards() : unflipCards();
+    }
+  }
+
+  const disableCards = () => {
+    setDisabledCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  const unflipCards = () => {
+    setUnflippedCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  const resetCards = () => {
+    setFirstCard({});
+    setSecondCard({});
+  }
+
+
 
   const listItems = cards.map((todo) => (
     <li className="cards" key={todo.name}>
       <div>
-        <Card kilber={todo.name} />
+        <Card 
+        kilber={todo.name} 
+        flipCard={flipCard}
+        unflippedCards={unflippedCards}
+        disabledCards={disabledCards} />
       </div>
     </li>
   ));
--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,37 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import ReactCardFlip from 'react-card-flip';
-
-// export default class card extends Component {
-//   render() {
-//     return (
-//       <div className="">
-
-//       </div>
-//     )
-//   }
-// }
 
 
 // Consumir API estructura y ruta para conseguir la imagen front_default
 
 // useState aplica depues que se corre la funcion y useEffect antes del async await
-const Card = (props) => {
+const Card = ({props, name, number, flipCard, unflippedCards, disabledCards}) => {
 
     // voltear Cartas
     const [isFlipped, setIsFlipped] = useState(false);
 
+    const [hasEvent, setHasEvent] = useState(true);
+
+    //ejecutar funcion para voltear cartas
+    
+    useEffect(() => {
+      if (unflippedCards.includes(number)) {
+        setTimeout(() => setIsFlipped(false), 700);
+      }
+    }, [unflippedCards])
+
+    useEffect(() => {
+      if (disabledCards.includes(number)) {
+        setHasEvent(false);
+      }
+    }, [disabledCards])
+
+
     const handleClick = e => {
-      setIsFlipped(!isFlipped);
+      const value = flipCard(name, number);
+      if (value !== 0) {
+        setIsFlipped(!isFlipped);
+      }
     }
   
     // fin voltear cartas 
@@ -32,6 +42,7 @@ const Card = (props) => {
   const fetchApi = async () => {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+props.kilber);
     const data = await response.json();
+    // Recorriendo json para encontrar las imegenes que se encuentran en el front_default
     setUrlimagen(data.sprites.other.home.front_default);
   }
   useState(() => {
@@ -44,8 +55,8 @@ const Card = (props) => {
       {/* <img className='cartas' src={urlimagen} />  {props.kilber}  */}
 
     <ReactCardFlip isFlipped={isFlipped}>
-      <img className='cartas' src={"https://cdn-icons-png.flaticon.com/512/6432/6432786.png"} alt='back-face' onClick={handleClick}/> 
-      <img className='cartas' src={urlimagen} alt='front-face' onClick={handleClick}/> 
+      <img className='cartas' src={"https://cdn-icons-png.flaticon.com/512/6432/6432786.png"} alt='back-face' onClick={hasEvent ? handleClick : null}/> 
+      <img className='cartas' src={urlimagen} alt='front-face' onClick={hasEvent ? handleClick : null}/> 
     </ReactCardFlip>
 
     </div>
